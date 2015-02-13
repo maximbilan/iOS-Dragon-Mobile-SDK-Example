a950c039977996801fac53262710f7b2aa8f8e30
Maxim Bilan <maximb.mail@gmail.com>
2015-02-13 11:23:53 +0200

    Added ObjectAL

---
+++ DragonMobileRecognizer/Pods/Target Support Files/Pods-ObjectAL-for-iPhone/Pods-ObjectAL-for-iPhone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ObjectAL_for_iPhone : NSObject
+@end
+@implementation PodsDummy_Pods_ObjectAL_for_iPhone
+@end
---
+++ DragonMobileRecognizer/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ObjectAL-for-iPhone
+#define COCOAPODS_POD_AVAILABLE_ObjectAL_for_iPhone
+#define COCOAPODS_VERSION_MAJOR_ObjectAL_for_iPhone 2
+#define COCOAPODS_VERSION_MINOR_ObjectAL_for_iPhone 4
+#define COCOAPODS_VERSION_PATCH_ObjectAL_for_iPhone 0
+
